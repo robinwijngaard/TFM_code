d39d69525a0f374e53926922f64c17a6379c54e9
robinwijngaard <robinwijngaard@gmail.com>
2021-03-13 09:28:10 +0100

    delete DeCON

--- DECoN-master/Linux/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ -w ".Rprofile" ] && rm .Rprofile
-
-/home/robin/Downloads/R-3.1.2/bin/Rscript sessionInfo.R --bootstrap-packrat > setup.log 2>&1
-
-cp packrat/packrat_source/.Rprofile ./
