d77e803ffc79c72a26f83ed348ced8bf7b4eadd9
robinwijngaard <robinwijngaard@gmail.com>
2021-03-13 13:30:30 +0100

    update rundecon script

--- FIles/rundecon.sh
+++ FIles/rundecon.sh
@@ -4,9 +4,17 @@ find ~/Documents/Project/Samples/example/bam_ex -name *.bam > bamfiles.txt
 
 #paths
 
-decon="/home/robin/Documents/Project/TFM_code/DECoN-master/Linux"
-bams="/home/robin/Documents/Project/TFM_code/FIles/"
+bams="/home/robin/Documents/Project/TFM_code/FIles"
 bed="/home/robin/Documents/Project/Samples/bedfiles"
 ref="/home/robin/Documents/Project/Samples/hg38ncbi"
 
-/home/robin/Downloads/R-3.1.2/bin/Rscript "$decon"/ReadInBams.R --bams "$bams"/bamfiles.txt --bed "$bed"/ICR96_hg38.bed --fasta "$ref"/hg38.fna --out DECoNtest
+
+##/home/robin/Documents/Project/TFM_code/DECoN-master/Linux/
+
+cd /home/robin/Documents/Project/TFM_code/DECoN-master/Linux/
+
+/home/robin/Downloads/R-3.1.2/bin/Rscript ReadInBams.R --bams "$bams"/bamfiles.txt --bed "$bed"/ICR96_hg38.bed --fasta "$ref"/hg38.fna --out DECoNtest
+
+
+#/home/robin/Downloads/R-3.1.2/bin/Rscript ReadInBams.R --bams /home/robin/Documents/Project/TFM_code/FIles/bamfile.txt --bed /home/robin/Documents/Project/Samples/bedfiles/ICR96_hg38.bed --fasta /home/robin/Documents/Project/Samples/hg38ncbi/hg38.fna --out DEConTest
+
