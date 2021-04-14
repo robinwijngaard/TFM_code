09ef517b8bb73449e504f6e5ec463cf72aae99d0
robinwijngaard <robinwijngaard@gmail.com>
2021-04-14 06:32:22 +0200

    run benchmark results

--- Files/Aligment/alignment.sh
+++ Files/Aligment/alignment.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-
 ## Define routes
-
 hg38="/home/robin/Documents/Project/Samples/hg38"
 fastq="/home/robin/Documents/Project/Samples/fastq"
 bam="/home/robin/Documents/Project/Samples/bam"
 alignmdir="/home/robin/Documents/Project/TFM_code/Files/Aligment"
 
-
 # Obtain filelist.txt: 
-
 ls "$fastq" | cat | sed 's/_.*//g' | sort | uniq > "$alignmdir"/fastqlist.txt
 
 #Obtain BAM
-
-
-
 for file in `cat "$alignmdir"/fastqlist.txt`
 do
 	R1=`echo $file | sed 's/$/_R1.fastq.gz/g'`
@@ -24,4 +17,3 @@ do
 	bwa mem -t 8 "$hg38"/hg38.fa "$fastq"/"$R1" "$fastq"/"$R2" | samtools sort -@ 8 -o "$bam"/"$file".bam -
 	samtools index "$bam"/"$file".bam
 done
-
--- Files/Manta/runmanta.sh
+++ Files/Manta/runmanta.sh
@@ -4,25 +4,24 @@ eval "$(conda shell.bash hook)"
 conda activate ~/anaconda3/envs/mantaenv
 
 #paths
-
-# Germline configuration examples
-
 bams="/home/robin/Documents/Project/Samples/example/bam_ex"
 bed="/home/robin/Documents/Project/Samples/bedfiles"
 ref="/home/robin/Documents/Project/Samples/hg38"
-
 manta_conf="/home/robin/Documents/Project/manta/Install"
 manta_exe="/home/robin/Documents/Project/Results/Manta"
 
+# number of open files
 ulimit -n 75000
 
 cd "$manta_exe"
 ls /home/robin/Documents/Project/Samples/example/bam_ex/*.bam > bamfiles.txt
 
+# array of samples
 declare -a lines
 readarray -t line <bamfiles.txt
 echo "${line[0]}"
 
+# manta configuration
 python "$manta_conf"/bin/configManta.py \
 --bam `echo "${line[0]}"` \
 --bam `echo "${line[1]}"` \
@@ -32,9 +31,19 @@ python "$manta_conf"/bin/configManta.py \
 --bam `echo "${line[5]}"` \
 --bam `echo "${line[6]}"` \
 --referenceFasta "$ref"/hg38.fa \
---runDir "$manta_exe"/"$1" \
+--runDir "$manta_exe" \
 --exome \
 --callRegions "$bed"/ICR96_hg38_noSNP.bed.gz
+
+# manta execution
+python "$manta_exe"/runWorkflow.py 
+
+
+
+
+
+
+
 
 #--bam `echo "${line[7]}"` \
 #--bam `echo "${line[8]}"` \
