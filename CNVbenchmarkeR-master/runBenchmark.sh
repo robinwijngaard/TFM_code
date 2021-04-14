eac2f5a90a85d8943cc80e67fa4fcbe05d8e807c
robinwijngaard <robinwijngaard@gmail.com>
2021-04-14 06:44:53 +0200

    results

--- CNVbenchmarkeR-master/runBenchmark.sh
+++ CNVbenchmarkeR-master/runBenchmark.sh
@@ -10,41 +10,41 @@ mkdir -p logs
 mkdir -p output
 
 # Execute algorithms over selected datasets
-if [ "$pars_algorithms_panelcn" == "true" ]; then
-    echo "[$(date)] Executing panelcn.MOPS"
-	Rscript ./algorithms/panelcnmops/runPanelcnmops.r ./algorithms/panelcnmops/panelcnmopsParams.yaml datasets.yaml  > logs/panelcnmops.log 2>&1
-fi
+#if [ "$pars_algorithms_panelcn" == "true" ]; then
+#    echo "[$(date)] Executing panelcn.MOPS"
+#	Rscript ./algorithms/panelcnmops/runPanelcnmops.r ./algorithms/panelcnmops/panelcnmopsParams.yaml datasets.yaml  > logs/panelcnmops.log 2>&1
+#fi
 
-if [ "$pars_algorithms_decon" == "true" ]; then
-    echo "[$(date)] Executing DECoN"
-	Rscript ./algorithms/decon/runDecon.r ./algorithms/decon/deconParams.yaml datasets.yaml  > logs/decon.log 2>&1
-fi
+#if [ "$pars_algorithms_decon" == "true" ]; then
+#    echo "[$(date)] Executing DECoN"
+#	Rscript ./algorithms/decon/runDecon.r ./algorithms/decon/deconParams.yaml datasets.yaml  > logs/decon.log 2>&1
+#fi
 
-if [ "$pars_algorithms_exomedepth" == "true" ]; then
-    echo "[$(date)] Executing ExomeDepth"
-	Rscript ./algorithms/exomedepth/runExomedepth.r ./algorithms/exomedepth/exomedepthParams.yaml datasets.yaml  > logs/exomedepth.log 2>&1
-fi
+#if [ "$pars_algorithms_exomedepth" == "true" ]; then
+#    echo "[$(date)] Executing ExomeDepth"
+#	Rscript ./algorithms/exomedepth/runExomedepth.r ./algorithms/exomedepth/exomedepthParams.yaml datasets.yaml  > logs/exomedepth.log 2>&1
+#fi
 
-if [ "$pars_algorithms_codex2" == "true" ]; then
-    echo "[$(date)] Executing CODEX2"
-	Rscript ./algorithms/codex2/runCodex2.r ./algorithms/codex2/codex2Params.yaml datasets.yaml  > logs/codex2.log 2>&1
-fi
+#if [ "$pars_algorithms_codex2" == "true" ]; then
+#    echo "[$(date)] Executing CODEX2"
+#	Rscript ./algorithms/codex2/runCodex2.r ./algorithms/codex2/codex2Params.yaml datasets.yaml  > logs/codex2.log 2>&1
+#fi
 
-if [ "$pars_algorithms_convading" == "true" ]; then
-    echo "[$(date)] Executing CoNVaDING"
-    export LC_ALL=en_US.UTF-8
-	Rscript ./algorithms/convading/runConvading.r ./algorithms/convading/convadingParams.yaml datasets.yaml  > logs/convading.log 2>&1
-fi
+#if [ "$pars_algorithms_convading" == "true" ]; then
+#    echo "[$(date)] Executing CoNVaDING"
+#    export LC_ALL=en_US.UTF-8
+#	Rscript ./algorithms/convading/runConvading.r ./algorithms/convading/convadingParams.yaml datasets.yaml  > logs/convading.log 2>&1
+#fi
 
-if [ "$pars_algorithms_manta" == "true" ]; then
-    echo "[$(date)] Executing Manta"
-    Rscript ./algorithms/manta/runManta.r ./algorithms/manta/mantaParams.yaml datasets.yaml  > logs/manta.log 2>&1
-fi
+#if [ "$pars_algorithms_manta" == "true" ]; then
+#    echo "[$(date)] Executing Manta"
+#    Rscript ./algorithms/manta/runManta.r ./algorithms/manta/mantaParams.yaml datasets.yaml  > logs/manta.log 2>&1
+#fi
 
-if [ "$pars_algorithms_cnvkit" == "true" ]; then
-    echo "[$(date)] Executing CNVkit"
-    Rscript ./algorithms/cnvkit/runCNVkit.r ./algorithms/cnvkit/cnvkitParams.yaml datasets.yaml  > logs/cnvkit.log 2>&1
-fi
+#if [ "$pars_algorithms_cnvkit" == "true" ]; then
+#    echo "[$(date)] Executing CNVkit"
+#    Rscript ./algorithms/cnvkit/runCNVkit.r ./algorithms/cnvkit/cnvkitParams.yaml datasets.yaml  > logs/cnvkit.log 2>&1
+#fi
 
 
 Generate summary file
